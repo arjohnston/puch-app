86d1b6c22c2b6232fd5f72517714a2d0a3f34316
Andrew Johnston <arjohnston3@gmail.com>
2024-02-19 08:52:39 -0800

    a (#6)

--- packages/core/src/Footer/index.tsx
+++ packages/core/src/Footer/index.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Header = () => {
   const commitHash = process.env.VERCEL_GIT_COMMIT_SHA ?? '<development>';
 
-  return <footer>{commitHash}</footer>;
+  return (
+    <footer>
+      <a
+        href={`https://github.com/arjohnston/puch-app/commit/${commitHash}`}
+        target='_blank'
+        rel='noreferrer noopener'
+      >
+        {commitHash}
+      </a>
+    </footer>
+  );
 };
 
 export default Header;
